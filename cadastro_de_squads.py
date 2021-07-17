8227709e648bc324d50aa5a4365cca4898a7c9fb
Victor Arcanjo de Oliveira <victor.arcanjo92@hotmail.com>
2021-07-17 20:03:52 -0300

    Segundo commit

--- cadastro_de_squads.py
+++ cadastro_de_squads.py
@@ -3,8 +3,24 @@ class Pessoa:
         self.nome = nome
         self.fone = fone
 
+
 class Squad:
     def __init__(self, nome, techlead=None, devs=None):
         self.nome = nome
         self.devs = []
         self.techlead = techlead
+
+    def incluir_techlead(self, techlead):
+            self.techlead = techlead
+
+
+class Colaborador(Pessoa):
+    def __init__(self, nome, fone, squad=None):
+        super().__init__(nome,fone)
+        self.squad = squad
+
+
+class Dev(Colaborador):
+    def __init__(self, nome, fone, cargo, squad=None):
+        super().__init__(nome, fone, squad)
+        self.cargo = cargo
